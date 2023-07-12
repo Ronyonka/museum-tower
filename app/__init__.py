69a41d696f24eed601e203bd429996a374cd4eaf
ronyonka <44805666+Ronyonka@users.noreply.github.com>
2023-07-12 06:45:28 +0300

    debugged problem causing flask-migrate not to properly work

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import DATABASE_NAME, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_HOST
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 db_user = DATABASE_USERNAME
@@ -11,5 +12,6 @@ db_host = DATABASE_HOST
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_password}@{db_host}/{db_name}'
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from app import routes
